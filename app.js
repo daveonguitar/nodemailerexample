904d056b6e59f01ba58d4623f2de04ea5df80f14
daveonguitar <duh_unabomb@hotmail.com>
2020-06-03 16:05:03 -0500

    initial commit

---
+++ app.js
@@ -0,0 +1,29 @@
+require("dotenv").config();
+
+const nodemailer = require("nodemailer");
+const log = console.log;
+
+//Step 1 - setting up a transporter
+let transporter = nodemailer.createTransport({
+  service: "gmail",
+  auth: {
+    user: process.env.EMAIL,
+    pass: process.env.PASSWORD,
+  },
+});
+
+//Step 2 - setting up the email message
+let mailOptions = {
+  from: "bensonda81@gmail.com",
+  to: "duh_unabomb@hotmail.com",
+  subject: "Is this thing on?",
+  text: "Is this thing on?",
+};
+
+//Step 3 - sending the message
+transporter.sendMail(mailOptions, (err, data) => {
+  if (err) {
+    return log("Error sending mail", err);
+  }
+  return log("Email sent!");
+});
